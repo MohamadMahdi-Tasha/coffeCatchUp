26e31c6ac5cbeedbdb6bb11a8bb87c6a4d4c570e
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-04-12 16:09:53 +0330

    added router and homepage

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
-// Importing Part
 // Codes By Mahdi Tasha
+// Importing Part
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from './pages/homePage';
 
 // Variables
 const rootElement:HTMLElement = document.getElementById('root') as HTMLElement;
@@ -10,7 +12,11 @@ const root:ReactDOM.Root = ReactDOM.createRoot(rootElement);
 
 // Rendering Whole App Into StickMode Wrapper And Rendering That Inside In Root We Created Above
 root.render(
-  <React.StrictMode>
-    <h1>asd</h1>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path={'/'} element={<HomePage />}/>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 )
