04b4cef85f7fdfbdcadc5e3e3a2e5af71f8c3f20
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-04-12 16:52:51 +0330

    added some styles for header

--- src/components/headerComponent.tsx
+++ src/components/headerComponent.tsx
@@ -8,15 +8,17 @@ import MyImage from './../assets/img/img-mahdi-tasha.jpg';
 export default function HeaderComponent():JSX.Element{
     // Returning JSX
     return(
-        <header>
-            <ContainerComponent>
-                <button><IconComponent type={'hamburger'}/></button>
+        <header className={'border-b border-b-slate-500/30'}>
+            <ContainerComponent className={'p-3 flex items-center justify-between'}>
+                <button className={'w-8 h-8 flex items-center justify-center text-black rounded-full'}><IconComponent type={'hamburger'}/></button>
                 <h1>Coffee CatchUp</h1>
-                <div>
-                    <img src={MyImage} alt="Mahdi Tasha"/>
-                    <button>
-                        <IconComponent type={'plus'}/>
-                        write
+                <div className={'flex items-center gap-8'}>
+                    <div className={'rounded-full p-0.5 border-2 border-2-slate-500/30'}>
+                        <img className={'w-8 h-8 rounded-full'} src={MyImage} alt="Mahdi Tasha"/>
+                    </div>
+                    <button className={'flex items-center gap-2 px-5 py-1 rounded-md border border-slate-500/30 hover:bg-slate-400/30 transition-colors ease-in-out'}>
+                        <span>+</span>
+                        <span>Add</span>
                     </button>
                 </div>
             </ContainerComponent>
