fe04fb224a2dba409bb379b364d07c0af06d051e
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-04-12 15:37:58 +0330

    added some changes in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
+// Importing Part
+// Codes By Mahdi Tasha
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+// Variables
+const rootElement:HTMLElement = document.getElementById('root') as HTMLElement;
+const root:ReactDOM.Root = ReactDOM.createRoot(rootElement);
+
+// Rendering Whole App Into StickMode Wrapper And Rendering That Inside In Root We Created Above
+root.render(
   <React.StrictMode>
     <h1>asd</h1>
   </React.StrictMode>,
