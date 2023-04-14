750795a7fc7a950ad7e5ad42dc817a0b7d2cc51a
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-04-14 12:27:53 +0330

    added styles and everything in left side nav

---
+++ src/components/chunks/articleItemsInNavComponent.tsx
@@ -0,0 +1,37 @@
+// Codes By Mahdi Tasha
+// Importing Part
+import { Link } from "react-router-dom";
+import MyImageComponent from "./myImageComponent";
+
+// Defining type of props
+interface openedInterface {
+    date: string;
+    img?: string;
+}
+
+// Exporting functional component as default
+export default function LeftSideNavComponent({date, img}:openedInterface):JSX.Element{
+    // variables
+    const selectedDate = new Date(date);
+    const yearOfSelectedDate = selectedDate.getFullYear();
+    const monthOfSelectedDate = selectedDate.getMonth();
+    const dateOfSelectedDate = selectedDate.getDate();
+
+    // Returning JSX
+    return(
+        <li>
+            <Link to={'#'}>
+                <article className={'md:flex block px-3 py-4 bg-transparent hover:bg-white/20 transition-all'}>
+                    <div className={'md:w-[100px] md:mr-2 mr-0 md:mb-0 mb-2 w-full md:h-[100px] h-[150px] bg-pink-600 rounded-lg'}></div>
+                    <div className={'md:flex block flex-col justify-between'}>
+                        <div className={'md:mb-0 mb-2'}>
+                            <h6 className={'text-white font-bold font-sm'}>{yearOfSelectedDate}/{monthOfSelectedDate}/{dateOfSelectedDate}</h6>
+                            <h6 className={'text-white/60 font-medium font-sm'}>Mahdi Tasha</h6>
+                        </div>
+                        <p className={'text-white/50 font-light font-sm'}>Click Here To See Report Of Day</p>
+                    </div>
+                </article>
+            </Link>
+        </li>
+    );
+}
--- src/components/leftSideNavComponent.tsx
+++ src/components/leftSideNavComponent.tsx
@@ -1,6 +1,8 @@
 // Codes By Mahdi Tasha
 // Importing Part
 import IconComponent from './chunks/iconComponent';
+import ArticleItemsInNavComponent from './chunks/articleItemsInNavComponent';
+import Logo from './../assets/img/img-logo.png';
 import { Link } from "react-router-dom";
 
 // Defining type of props
@@ -12,10 +14,27 @@ export default function LeftSideNavComponent({isOpened}:openedInterface):JSX.Ele
     return(
         <>
             <div data-opened={isOpened} className={'absolute data-[opened="true"]:opacity-1 data-[opened="false"]:opacity-0 left-0 top-0 w-full h-full bg-black/30 backdrop-blur-md transition-all'} />
-            <nav data-opened={isOpened} className={'absolute data-[opened="true"]:left-0 md:data-[opened="false"]:left-[-30%] data-[opened="false"]:-left-[-70%] top-0 md:w-[30%] w-[70%] h-full p-3 bg-indigo-600 transition-all'}>
-                <a href="https://www.github.com/MohamadMahdi-Tasha"><button><IconComponent type={'github'}/>My Github</button></a>
-                <Link to={'#'}><button>2023/12/6</button></Link>
-                <Link to={'#'}><button>2023/12/6</button></Link>
+            <nav data-opened={isOpened} className={'absolute overflow-auto data-[opened="true"]:left-0 md:data-[opened="false"]:left-[-30%] data-[opened="false"]:-left-[-70%] top-0 md:w-[30%] w-[70%] h-full bg-indigo-600 transition-all'}>
+                <Link to={'/'} className={'bg-black flex justify-center items-center p-5'}>
+                    <img src={Logo} alt="Coffe CatchUp Logo" width={'50px'} height={'50px'}/>
+                </Link>
+                <a className={'block border-y border-y-white/30'} href="https://www.github.com/MohamadMahdi-Tasha">
+                    <button tabIndex={-1} className={'flex items-center p-3 w-full text-white hover:bg-white/20 transition-all'}>
+                        <IconComponent type={'github'}/>
+                        <span className={'ml-2'}>My Github</span>
+                    </button>
+                </a>
+                <div>
+                    <h6 className={'m-3 text-white font-semibold text-lg'}>Latest Reports:</h6>
+                    <ul className={'border-b border-b-white/20'}>
+                        <ArticleItemsInNavComponent date={'2023/12/6'} img={'s'}/>
+                        <ArticleItemsInNavComponent date={'2023/12/6'} img={'s'}/>
+                    </ul>
+                </div>
+                <div className={'p-3'}>
+                    <h6 className={'text-white font-semibold text-lg block mb-3'}>Find Reports On Date :</h6>
+                    <input className={'w-full bg-transparent border cursor-pointer border-white/20 p-3 rounded-md text-white hover:bg-white/20 transition-all'} type="date"/>
+                </div>
             </nav>
         </>
     );
