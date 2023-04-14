f770ccac7e1cb09d8a70d68c1f0147707b95ae00
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-04-14 16:21:19 +0330

    like last commit

--- src/components/chunks/activitiesDropDownComponent.tsx
+++ src/components/chunks/activitiesDropDownComponent.tsx
@@ -38,13 +38,13 @@ export default function ActivitiesDropDownComponent({placeHolder}:placeHolderTyp
     // Returning JSX
     return(
         <div className={'relative'}>
-            <div onClick={() => (isOpened) ? setOpened(false) : setOpened(true)} className={`block cursor-pointer border-2 ${(isOpened) ? 'border-indigo-300': 'border-black/20' } outline-0 ${(isOpened) ? 'bg-indigo-300': 'bg-black/20'} focus:bg-indigo-300 focus:border-indigo-300 text-black/60 font-roboto text-black w-full p-3 h-[62px] overflow-auto rounded-md mb-4 text-start font-normal text-sm my-transition flex flex-wrap gap-3`}>
+            <button type={'button'} onClick={() => (isOpened) ? setOpened(false) : setOpened(true)} className={`block cursor-pointer border-2 ${(isOpened) ? 'border-indigo-300': 'border-black/20' } outline-0 ${(isOpened) ? 'bg-indigo-300': 'bg-black/20'} focus:bg-indigo-300 focus:border-indigo-300 text-black/60 font-roboto text-black w-full p-3 h-[62px] overflow-auto rounded-md mb-4 text-start font-normal text-sm my-transition flex flex-wrap gap-3`}>
                 {
                     (isTypesSelected)
                         ? selectedTypes.map((type:string,index:number):any => <DaysTypeTextWithoutSelectingComponent key={index} type={type}/>)
                         : placeHolder
                 }
-            </div>
+            </button>
             <div data-opened={isOpened} className={'absolute data-[opened="false"]:invisible outline-0 outline-white data-[opened="false"]:visible data-[opened="false"]:opacity-0 data-[opened="false"]:opacity-1 data-[opened="false"]:top-[20px] data-[opened="true"]:top-0 my-transition translate-y-[-90%] bg-white w-full bg-white shadow rounded-lg p-3 flex flex-wrap gap-3'}>
                 <DaysTypeButtonWithoutSelectingComponent onClick={handleClickOfTypeButtons} type={'lazy'}/>
                 <DaysTypeButtonWithoutSelectingComponent onClick={handleClickOfTypeButtons} type={'productive'}/>
